38c90b2d43b251621a6971f819844e3cd76d4cd1
halaszk <halaszk@deathlord.eu>
2013-04-28 11:29:34 +0200

    my mistake fix..

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -744,7 +744,7 @@ int power_mode = PM_MAX;
 				/* Kernel resumed  */
 				DHD_ERROR(("%s: Remove extra suspend setting\n", __FUNCTION__));
 
-CONFIG_BCMDHD_WIFI_PM
+#if CONFIG_BCMDHD_WIFI_PM
 				dhd_wl_ioctl_cmd(dhd, WLC_SET_PM, (char *)&power_mode, sizeof(power_mode), TRUE, 0);
 #else
 #ifndef SUPPORT_PM2_ONLY
