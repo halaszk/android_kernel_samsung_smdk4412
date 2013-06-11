21545b4ea9f5266f4056acdc61d1a6014488074a
halaszk <halaszk@deathlord.eu>
2013-06-11 19:15:26 +0200

    Added N7100 full support and separate build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
+TARGET=$1
+if [ "$TARGET" != "" ]; then
+echo "starting your build for $TARGET"
+else
+echo ""
+echo "you need to define your device target!"
+echo "example: build_kernel.sh n7100"
+exit 1
+fi
+
 # location
 export KERNELDIR=`readlink -f .`
 export PARENT_DIR=`readlink -f ..`
+
+if [ "$TARGET" = "i9300" ] ; then
 export INITRAMFS_SOURCE=`readlink -f $KERNELDIR/../initramfs3`
+else
+export INITRAMFS_SOURCE=`readlink -f $KERNELDIR/../ramfs-n7100`
+fi;
 
 # kernel
 export ARCH=arm
 export USE_SEC_FIPS_MODE=true
-export KERNEL_CONFIG="halaszk_defconfig"
+
+if [ "$TARGET" = "i9300" ] ; then
+export KERNEL_CONFIG="halaszk_i9300_defconfig"
+else
+export KERNEL_CONFIG="halaszk_n7100_defconfig"
+fi;
 
 # build script
 export USER=`whoami`
 # gcc 4.7.3 (Linaro 13.02)
 export CROSS_COMPILE=${KERNELDIR}/android-toolchain/bin/arm-eabi-
-#export CROSS_COMPILE=$PARENT_DIR/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
-#export CROSS_COMPILE=/home/halaszk/android_build/SGS2/kernel/Dorimanx-SG2-I9100-Kernel/linaro-12-android-toolchain/bin/arm-eabi-
-#export CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf-
-if [ "${1}" != "" ];then
-export KERNELDIR=`readlink -f ${1}`
-fi
 
+#if [ "${1}" != "" ];then
+#export KERNELDIR=`readlink -f ${1}`
+#fi
+#export KERNELDIR=`$PWD`
 # Importing PATCH for GCC depend on GCC version.
 GCCVERSION_OLD=`${CROSS_COMPILE}gcc --version | cut -d " " -f3 | cut -c3-5 | grep -iv "09" | grep -iv "ee" | grep -iv "en"`
 GCCVERSION_NEW=`${CROSS_COMPILE}gcc --version | cut -d " " -f4 | cut -c1-3 | grep -iv "Fre" | grep -iv "sof" | grep -iv "for" | grep -iv "auc"`
@@ -133,7 +151,7 @@ nice -n 10 make -j2 zImage || exit 1
 
 ${KERNELDIR}/mkshbootimg.py ${KERNELDIR}/boot.img ${KERNELDIR}/boot.img.pre ${KERNELDIR}/payload.tar
 rm -f ${KERNELDIR}/boot.img.pre
-
+if [ "$TARGET" = "i9300" ] ; then
 	# copy all needed to ready kernel folder.
 cp ${KERNELDIR}/.config ${KERNELDIR}/arch/arm/configs/${KERNEL_CONFIG}
 cp ${KERNELDIR}/.config ${KERNELDIR}/READY/
@@ -153,6 +171,27 @@ rm ${KERNELDIR}/READY/.config
 			ncftpput -f /home/halaszk/login.cfg -V -R / ${KERNELDIR}/SGSIII/
 			rm ${KERNELDIR}/SGSIII/Kernel_*
 			echo "Uploading kernel to FTP server DONE";
-
                 fi;
+else
+        # copy all needed to ready kernel folder.
+cp ${KERNELDIR}/.config ${KERNELDIR}/arch/arm/configs/${KERNEL_CONFIG}
+cp ${KERNELDIR}/.config ${KERNELDIR}/READY/
+rm ${KERNELDIR}/READY/boot/zImage
+rm ${KERNELDIR}/READY/Kernel_*
+stat ${KERNELDIR}/boot.img
+cp ${KERNELDIR}/boot.img /${KERNELDIR}/READY/boot/
+cd ${KERNELDIR}/READY/
+        zip -r Kernel_${GETVER}-`date +"[%H-%M]-[%d-%m]-JB-N7100-PWR-CORE"`.zip .
+rm ${KERNELDIR}/boot.img
+rm ${KERNELDIR}/READY/boot/boot.img
+rm ${KERNELDIR}/READY/.config
+                read -p "push kernel to ftp (y/n)?"
+                if [ "$REPLY" == "y" ]; then
+                        echo "Uploading kernel to FTP server";
+                        mv ${KERNELDIR}/READY/Kernel_* ${KERNELDIR}/N7100/
+                        ncftpput -f /home/halaszk/login.cfg -V -R / ${KERNELDIR}/N7100/
+                        rm ${KERNELDIR}/N7100/Kernel_*
+                        echo "Uploading kernel to FTP server DONE";
+fi;
+fi;
 
